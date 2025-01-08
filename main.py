f83491498c1afc0148d026b0b45908ff0ceec411
Billy <william.roecklein@gmail.com>
2025-01-07 18:25:37 -0800

    Added word count and printing of whole text

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    num_words = word_count(text)
     print(text)
+    print(f"{num_words} words found in the document")
+
+def word_count(text):
+    words = text.split()
+    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
