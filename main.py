97249b292d4a690f0f178af5e4d4872698b45e90
Billy <william.roecklein@gmail.com>
2025-01-07 17:59:24 -0800

    creating bookbot, adding source code

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
