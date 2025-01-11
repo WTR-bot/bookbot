bf251bc698b559883f110a3f387fe4d207c0ad33
Billy <william.roecklein@gmail.com>
2025-01-10 17:00:50 -0800

    D: added letter count

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     num_words = word_count(text)
     print(text)
     print(f"{num_words} words found in the document")
+    print(letter_count(text))
 
 def word_count(text):
     words = text.split()
@@ -12,5 +13,13 @@ def word_count(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def letter_count(text):
+    text = text.lower()
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    counts = {}
+    for letter in letters:
+        counts[letter] = text.count(letter)
+    return counts
     
 main()
