4aede21a99da4a86b6fbdd9150459af895c9de59
Billy <william.roecklein@gmail.com>
2025-01-14 18:28:45 -0800

    D: Added an aggregate report

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
-    print(text)
-    print(f"{num_words} words found in the document")
-    print(letter_count(text))
+    letter_counts = letter_count(text)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    
+    for letter, count in sorted(letter_counts.items(), key=lambda item: item[1], reverse=True):
+        print(f"The '{letter}' character was found {count} times")
+    
+    print("--- End report ---")
 
 def word_count(text):
     words = text.split()
     return len(words)
 
 def get_book_text(path):
-    with open(path) as f:
+   with open(path) as f:
         return f.read()
 
 def letter_count(text):
@@ -21,5 +27,5 @@ def letter_count(text):
     for letter in letters:
         counts[letter] = text.count(letter)
     return counts
-    
+
 main()
